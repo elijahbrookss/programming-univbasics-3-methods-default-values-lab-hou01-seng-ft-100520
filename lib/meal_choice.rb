d3aaf82704c609012d0fe2fd617959a7152de0ed
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-17 14:07:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,6 +5,6 @@ def meal_choice (veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   message2 = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts message2
-  message2learn
+  message2
   
 end
